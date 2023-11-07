67706c9fb200a0ac6af7e7610ef2baeb67095568
Rohan Godha <rgodha24@brophybroncos.org>
2023-11-07 10:04:15 -0700

    final

--- src-rs/materials/mod.rs
+++ src-rs/materials/mod.rs
@@ -15,13 +15,12 @@ impl_absorber!(
 
 impl_reflective!(
     AF, "Aluminum Foil", 0.55, 0.7;
-    // TF, "Tin Foil", 0.68, 0.88;
     MS, "Mirror Sheets", 23.24, 0.9;
     RT, "Reflective Tape", 19.35, 0.85;
     // S, "Silver", 79.36, 0.964;
     // RV, "Reflective Vinyl", 16.55, 0.8;
-    SR2000, "S Reflect 2000", 29.95, 0.92;
-    SRV, "Silver Reflective Vinyl", 13.40, 0.86
+    // SRV, "Silver Reflective Vinyl", 13.40, 0.86;
+    SR2000, "S Reflect 2000", 29.95, 0.92
 );
 
 impl_insulator!(
@@ -34,7 +33,7 @@ impl_insulator!(
 
 impl_body!(
     C, "Cardboard", 1.75, 0.064, 0.004;
-    W5, "Wood .5in", 2.48, 0.15, 0.5;
-    W2, "Wood .2in", 2.48, 0.15, 0.2;
+    // W5, "Wood .5in", 2.48, 0.15, 0.5;
+    // W2, "Wood .2in", 2.48, 0.15, 0.2;
     W16, "Wood 1/16 in", 2.48, 0.15, (16f64).recip()
 );
